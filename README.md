fe988d709e94257f164cb760e29c0b577744d48e
kellysutton <michael.k.sutton@gmail.com>
2015-06-06 11:20:23 -0700

    Create an ember shim for imgix-core-js

---
+++ README.md
@@ -0,0 +1,3 @@
+# ember-cli-imgix-core-js-shim
+
+A simple shim to adapt [imgix-core-js](https://github.com/imgix/imgix-core-js) for ember-cli.
---
+++ imgix-core-js-shim.js
@@ -0,0 +1,10 @@
+(function () {
+/* global define, Imgix */
+define('imgix-core-js', [], function() {
+  "use strict";
+
+  return {
+    'default': Imgix.Client
+  };
+});
+})();
